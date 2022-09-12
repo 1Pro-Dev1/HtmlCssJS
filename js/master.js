f7f45b2b5f0f5982f753c8219a289aa8cf9cb7be
1Mohamed-Dev <dude.alt.mail159@gmail.com>
2022-09-12 12:46:47 +0400

    Background Randomizer Option

--- js/master.js
+++ js/master.js
@@ -1,16 +1,3 @@
-/*
-let landing = document.querySelector(".landing");
-
-let imgs = ["landing1.jpg", "landing2.jpg", "landing3.jpg", "landing4.jpg", "landing5.jpg"];
-
-
-setInterval(function (){
-    let randomNum = Math.floor(Math.random() * imgs.length);
-    console.log(randomNum)
-    landing.style.backgroundImage = `url(img/${imgs[randomNum]})`;
-}, 3000)
-*/
-
 
 let gear = document.querySelector(".settings .gear i");
 let localColor = localStorage.getItem("color")
@@ -28,20 +15,33 @@ if (localColor !== null) {
     
 };
 
+let settings = document.querySelector(".settings");
+
 gear.onclick = function (){
     this.classList.toggle("fa-spin");
 
-    let settings = document.querySelector(".settings");
-
     settings.classList.toggle("open")
+
+    this.parentElement.classList.toggle("open")
 }
+
+let closer = document.querySelector(".settings .close");
+
+closer.onclick = function () {
+    settings.classList.toggle("open");
+    gear.classList.toggle("fa-spin");
+    gear.parentElement.classList.toggle("open");
+};
+
+const clickSound = new Audio();
+clickSound.src = "./media/click.wav"
 
 const colors = document.querySelectorAll(".settings .option-box .colors-list li");
 
 
 colors.forEach(li => {
     li.addEventListener("click", (e) => {
-        console.log(e.target.dataset.color);
+        clickSound.play();
 
         document.documentElement.style.setProperty("--main-color", e.target.dataset.color);
         localStorage.setItem("color", e.target.dataset.color);
@@ -52,3 +52,70 @@ colors.forEach(li => {
         e.target.classList.add("active")
     })
 })
+
+let backgroundOpt = true;
+let bcint;
+let backStorage = localStorage.getItem("Background_Randomize")
+
+if (backStorage !== null) {
+    console.log("Not Empty")
+
+    if (backStorage === "true") {
+        backgroundOpt = true;
+    }
+
+    if (backStorage === "false") {
+        backgroundOpt = false;
+    }
+
+    document.querySelectorAll(".settings .random span").forEach(element =>{
+        element.classList.remove("active")
+    })
+
+    if (backStorage === "true") {
+        document.querySelector(".random .enable").classList.add("active")
+
+    } else {
+        document.querySelector(".random .disable").classList.add("active")
+    }
+}
+
+if(backgroundOpt === true){
+    randomFuncion();
+}
+
+
+const randomBackEl = document.querySelectorAll(".settings .random span");
+
+randomBackEl.forEach(span => {
+    span.addEventListener("click", (e) => {
+        clickSound.play();
+
+        e.target.parentElement.querySelectorAll(".active").forEach(element => {
+            element.classList.remove("active")
+        })
+        e.target.classList.add("active")
+        if (e.target.dataset.back === "enable") {
+            backgroundOpt = true;
+            randomFuncion();
+            localStorage.setItem("Background_Randomize", true)
+        } else {
+            backgroundOpt = false;
+            clearInterval(bcint)
+            localStorage.setItem("Background_Randomize", false)
+        }
+    })
+})
+
+
+let landing = document.querySelector(".landing");
+
+let imgs = ["landing1.jpg", "landing2.jpg", "landing3.jpg", "landing4.jpg", "landing5.jpg"];
+
+function randomFuncion() {
+    bcint = setInterval(function (){
+        let randomNum = Math.floor(Math.random() * imgs.length);
+        landing.style.backgroundImage = `url(img/${imgs[randomNum]})`;
+    }, 5000)
+}
+
